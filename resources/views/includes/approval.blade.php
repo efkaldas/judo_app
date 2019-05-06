0aef395572db4ecf7a325803a19dba955c8b7be3
efkaldas <ev.kuslevic@gmail.com>
2019-05-06 14:01:49 +0300

    changed approval text

--- resources/views/includes/approval.blade.php
+++ resources/views/includes/approval.blade.php
@@ -5,12 +5,12 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Waiting for Approval</div>
+                    <div class="card-header">Laukiama patvirtinimo</div>
 
                     <div class="card-body">
-                        Your account is waiting for our administrator approval.
+                        Jūsų registracija yra perduota administratoriui, prašome palaukti patvirtinimo.
                         <br />
-                        Please check back later.
+                        Prašome patikrinti veliau.
                     </div>
                 </div>
             </div>
